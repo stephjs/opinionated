51cdb09396ef23cf693244105c3a3f4af09d2e1b
Stephanie Orpilla <stephanieorpilla@yahoo.com>
2018-06-02 19:02:10 -0700

    Remove node_modules

--- node_modules/graphql-tag/parser.js
+++
@@ -1,9 +0,0 @@
-var parser = require('./bundledParser');
-
-console.warn(
-  "Warning - the `parser` exports from `graphql-tag` will be removed in the next major version." +
-  "\nSee https://github.com/apollographql/graphql-tag/issues/54 for more information."
-);
-
-module.exports = parser;
-
